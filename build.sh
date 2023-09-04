c82479b5bb8fb14a683f9c9df71ac9041943c85d
nokian5500 <nokian5500@gmail.com>
2023-09-04 04:53:43 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,13 @@
 set -euo pipefail
 set -x
 
-if [ $# -ne 1 ]; then
-  echo "$0 <must supply nanominer version eg: 1.2.3>"
-  exit 1
-fi
 
-DOCKER_project_namespace='dmcanally'
+DOCKER_project_namespace='myhomemail565'
 DOCKER_app_name='nanominer'
-VER=$1
-DOCKER_image_tag_ver=$DOCKER_project_namespace/$DOCKER_app_name:$VER
+DOCKER_image_tag_ver=$DOCKER_project_namespace/$DOCKER_app_name:3.8.5
 DOCKER_image_tag_latest=$DOCKER_project_namespace/$DOCKER_app_name
 
-docker build --pull --no-cache --build-arg "VER=$VER" -t "$DOCKER_image_tag_latest" -t "$DOCKER_image_tag_ver" .
+docker build --pull --no-cache --build-arg "VER=3.8.5" -t "$DOCKER_image_tag_latest" -t "$DOCKER_image_tag_ver" .
 docker push $DOCKER_image_tag_latest
 docker push $DOCKER_image_tag_ver
 printf "$DOCKER_image_tag_ver\n" > .manifest
